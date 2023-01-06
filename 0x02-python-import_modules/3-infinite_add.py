8b28e4ac8fb1aa65ee6f6fd9550a6892664b7a5c
marto-g <gathimartin@gmail.com>
2023-01-06 08:05:37 -0800

    task2

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
 
     value = 0
     for i in range(len(sys.argv) - 1):
-         value = value + int(sys.argv[i + 1])
+        value = value + int(sys.argv[i + 1])
     print("{}".format(value))
