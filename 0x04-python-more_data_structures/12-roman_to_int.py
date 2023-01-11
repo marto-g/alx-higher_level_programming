141876537c52946c710913fabd7269fcc45bb439
marto-g <gathimartin@gmail.com>
2023-01-11 06:46:03 -0800

    task

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -14,8 +14,8 @@ def roman_to_int(roman_string):
                 roman_d[roman_string[i]] < roman_d[roman_string[i + 1]]):
                     number += roman_d[roman_string[i]] * -1
 
-        else:
-            number += roman_d[roman_string[i]]
+                else:
+                    number += roman_d[roman_string[i]]
     return (number)
 
 
