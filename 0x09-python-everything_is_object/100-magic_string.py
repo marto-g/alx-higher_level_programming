03d980db04071270667cfdb4377f277979fba456
marto-g <gathimartin@gmail.com>
2023-01-31 07:42:46 -0800

    29

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string():
-    magic_string.ntimes = getattr(magic_string, 'ntimes', 0) + 1
-    return ("Holberton, " * (magic_string.ntimes - 1) + "Holberton")
+def magic_string(H=[]):
+    H += ["BestSchool"]
+    return (", ".join(H))
